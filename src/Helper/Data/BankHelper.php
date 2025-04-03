f85422174676e28a946b7f2e1e3638461d78abaf
Daniel Schuppelius <daniel.schuppelius@gmail.com>
2025-04-03 21:40:41 +0200

    more countries for iban

--- src/Helper/Data/BankHelper.php
+++ src/Helper/Data/BankHelper.php
@@ -55,7 +55,7 @@ class BankHelper {
         $countries = self::countryLengths();
         $chars = self::ibanCharMap();
 
-        $countryCode = strtolower(substr($iban, 0, 2));
+        $countryCode = substr($iban, 0, 2);
         if (!isset($countries[$countryCode]) || strlen($iban) !== $countries[$countryCode]) {
             return false;
         }
@@ -84,7 +84,7 @@ class BankHelper {
     public static function generateIBAN(string $countryCode, string $accountNumber): string {
         self::requireBcMath();
 
-        $countryCode = strtolower($countryCode);
+        $countryCode = strtoupper($countryCode);
         $countries = self::countryLengths();
         $chars = self::ibanCharMap();
 
@@ -160,38 +160,73 @@ class BankHelper {
 
     private static function countryLengths(): array {
         return [
-            'de' => 22,
-            'at' => 20,
-            'ch' => 21,
-            'nl' => 18,
-            'be' => 16,
-            'fr' => 27,
-            'it' => 27,
-            'es' => 24,
-            'gb' => 22,
-            'pl' => 28,
-            'se' => 24,
-            'fi' => 18,
-            'pt' => 25,
-            'dk' => 18,
-            'ie' => 22,
-            'no' => 15,
-            'cz' => 24,
-            'sk' => 24,
-            'hu' => 28,
-            'lt' => 20,
-            'lu' => 20,
-            'si' => 19,
-            'lv' => 21,
-            'ee' => 20,
-            'ro' => 24,
-            'bg' => 22,
-            'hr' => 21,
-            'cy' => 28,
-            'mt' => 31,
-            'li' => 21,
-            'is' => 26,
-            'tr' => 26
+            'AL' => 28, // Albanien
+            'AD' => 24, // Andorra
+            'AT' => 20, // Österreich
+            'AZ' => 28, // Aserbaidschan
+            'BH' => 22, // Bahrain
+            'BE' => 16, // Belgien
+            'BA' => 20, // Bosnien und Herzegowina
+            'BR' => 29, // Brasilien
+            'BG' => 22, // Bulgarien
+            'CR' => 22, // Costa Rica
+            'HR' => 21, // Kroatien
+            'CY' => 28, // Zypern
+            'CZ' => 24, // Tschechien
+            'DK' => 18, // Dänemark
+            'DO' => 28, // Dominikanische Republik
+            'EE' => 20, // Estland
+            'FO' => 18, // Färöer
+            'FI' => 18, // Finnland
+            'FR' => 27, // Frankreich
+            'GE' => 22, // Georgien
+            'DE' => 22, // Deutschland
+            'GI' => 23, // Gibraltar
+            'GR' => 27, // Griechenland
+            'GL' => 18, // Grönland
+            'GT' => 28, // Guatemala
+            'HU' => 28, // Ungarn
+            'IS' => 26, // Island
+            'IE' => 22, // Irland
+            'IL' => 23, // Israel
+            'IT' => 27, // Italien
+            'JO' => 30, // Jordanien
+            'KZ' => 20, // Kasachstan
+            'XK' => 20, // Kosovo
+            'KW' => 30, // Kuwait
+            'LV' => 21, // Lettland
+            'LB' => 28, // Libanon
+            'LI' => 21, // Liechtenstein
+            'LT' => 20, // Litauen
+            'LU' => 20, // Luxemburg
+            'MT' => 31, // Malta
+            'MR' => 27, // Mauretanien
+            'MU' => 30, // Mauritius
+            'MD' => 24, // Moldawien
+            'MC' => 27, // Monaco
+            'ME' => 22, // Montenegro
+            'NL' => 18, // Niederlande
+            'NO' => 15, // Norwegen
+            'PK' => 24, // Pakistan
+            'PS' => 29, // Palästina
+            'PL' => 28, // Polen
+            'PT' => 25, // Portugal
+            'QA' => 29, // Katar
+            'RO' => 24, // Rumänien
+            'SM' => 27, // San Marino
+            'SA' => 24, // Saudi-Arabien
+            'RS' => 22, // Serbien
+            'SK' => 24, // Slowakei
+            'SI' => 19, // Slowenien
+            'ES' => 24, // Spanien
+            'SE' => 24, // Schweden
+            'CH' => 21, // Schweiz
+            'TN' => 24, // Tunesien
+            'TR' => 26, // Türkei
+            'AE' => 23, // Vereinigte Arabische Emirate
+            'GB' => 22, // Großbritannien
+            'VG' => 24, // Britische Jungferninseln
+            // TODO: Add more countries
         ];
     }
 
